2551c2c4f20978acdaa76977bf1b76190eb46a6b
byronPantoja <byronpantoja@gmail.com>
2019-07-27 05:32:16 +0800

    adding search form and some html styling to results page

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const port = 3000;
 
 app.set('view engine', 'ejs');
 
-app.get('/results', (req, res) =>
-	request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb', function(
-		error,
-		response,
-		body
-	) {
+app.get('/', function(req, res) {
+	res.render('search'); //renders to route page
+});
+
+app.get('/results', (req, res) => {
+	const query = req.query.search;
+	let url = 'http://www.omdbapi.com/?s=' + query + '&apikey=thewdb';
+	request(url, function(error, response, body) {
 		const data = JSON.parse(body);
 		res.render('results', { data: data });
-	})
-);
+	});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
